5ca1efc1850d10ccdb0fe1d223f64456757e7136
dilara-s <sagadieva.d2017@yandex.ru>
2023-12-19 22:50:02 +0300

    Добавлена функция

--- src/main/java/ru/itis/inf304/Main.java
+++ src/main/java/ru/itis/inf304/Main.java
@@ -3,5 +3,6 @@ package ru.itis.inf304;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("skjgbns;jbn");
     }
 }
